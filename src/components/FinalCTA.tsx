ed9aea61af9b525e115d57388c019feee7c3610a
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2025-10-01 17:26:25 -1000

    Updated FinalCTA.tsx

--- src/components/FinalCTA.tsx
+++ src/components/FinalCTA.tsx
@@ -11,7 +11,6 @@ const FinalCTA: React.FC = () => {
             <img 
               src="/public/logo-full.png"
               alt="Web3SetGo Logo"
-              className="w-16 h-16"
               />
           </div>
           
