824c3801fe8ebbc5dfec601033c055716fff4d26
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2025-10-05 11:15:46 -1000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import ScrollToTop from './components/ScrollToTop';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import LearningPage from './pages/LearningPage';
@@ -15,16 +16,19 @@ function App() {
     <Router>
       <div className="min-h-screen bg-slate-50 font-inter">
         <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/learning" element={<LearningPage />} />
-          <Route path="/learning/:slug" element={<ArticlePage />} />
-          <Route path="/blogs" element={<BlogsPage />} />
-          <Route path="/blogs/new" element={<NewBlogPage />} />
-          <Route path="/blogs/:slug" element={<ArticlePage />} />
-          <Route path="/quiz" element={<QuizPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <ScrollToTop />
+        <div className="pt-16">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/learning" element={<LearningPage />} />
+            <Route path="/learning/:slug" element={<ArticlePage />} />
+            <Route path="/blogs" element={<BlogsPage />} />
+            <Route path="/blogs/new" element={<NewBlogPage />} />
+            <Route path="/blogs/:slug" element={<ArticlePage />} />
+            <Route path="/quiz" element={<QuizPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
