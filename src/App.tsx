4ea7a12b0a6e54c0b5a102cf5a71076ad89762c2
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2025-10-13 10:22:55 -1000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ScrollToTop from './components/ScrollToTop';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import BlogsPage from './pages/BlogsPage';
-import ViewBlogPage from './pages/ViewBlogPage';
+import BlogPostPage from './pages/BlogPostPage';
 import NewBlogPage from './pages/NewBlogPage';
 import QuizPage from './pages/QuizPage';
 import SimulationsPage from './pages/SimulationsPage';
@@ -24,7 +24,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/blogs" element={<BlogsPage />} />
             <Route path="/blogs/new" element={<RequireAdmin><NewBlogPage /></RequireAdmin>} />
-            <Route path="/blogs/:slug" element={<ViewBlogPage />} />
+            <Route path="/blogs/:slug" element={<BlogPostPage />} />
             <Route path="/quiz" element={<QuizPage />} />
             <Route path="/simulations" element={<SimulationsPage />} />
             <Route path="/simulations/:slug" element={<SimulationPage />} />
