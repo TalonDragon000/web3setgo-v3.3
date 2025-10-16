cc9ba00e01146e21254f739be285937e7f462f31
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2025-10-15 14:40:34 -1000

    Updated QuizPage.tsx

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -302,11 +302,6 @@ const QuizPage: React.FC = () => {
               <ArrowLeft className="h-5 w-5 mr-2" />
               Back to Quizzes
             </Link>
-
-            <div className="flex items-center space-x-2">
-              <Target className="h-6 w-6 text-ocean-500" />
-              <span className="text-xl font-semibold text-gray-900">Quizzes</span>
-            </div>
           </div>
       </header>
 
